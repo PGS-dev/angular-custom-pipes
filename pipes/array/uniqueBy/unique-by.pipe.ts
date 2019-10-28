fc77af2170e4464b9626d1cb944932ea62901510
tlaskowski <tlaskowski@pgs-soft.com>
2019-10-28 07:42:29 +0100

    PGS dev: add sum pipe

--- pipes/array/uniqueBy/unique-by.pipe.ts
+++ pipes/array/uniqueBy/unique-by.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UniqueByPipe implements PipeTransform {
   transform(array: any[], key: string): any[] {
-    return array.filter((item: any, i: number, result: any[]) => result.findIndex(resultItem => resultItem[key] === item[key]) === i);
+    return array.filter((item: any, index: number, result: any[]) => result.findIndex(resultItem => resultItem[key] === item[key]) === index);
   }
 }
